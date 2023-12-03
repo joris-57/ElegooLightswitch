13e9cb8f736f8a66a11ba1dc35e8254381c0e8a8
joris-57 <152792878+joris-57@users.noreply.github.com>
2023-12-03 18:16:43 +0100

    Create framelighttoggle.sh

---
+++ Scripts/framelighttoggle.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Set the GPIO pin number
+GPIO_PIN=66
+# Read the current value of the GPIO pin
+CURRENT_VALUE=$(cat /sys/class/gpio/gpio$GPIO_PIN/active_low)
+# Flip the GPIO pin
+if [ $CURRENT_VALUE -eq 1 ]
+then
+    echo 0 | tee /sys/class/gpio/gpio$GPIO_PIN/active_low > /dev/null
+else
+    echo 1 | tee /sys/class/gpio/gpio$GPIO_PIN/active_low > /dev/null
+fi
